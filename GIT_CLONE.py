37921f46fde5ce18c428c68162e2af7190a75d04
Kim yeong bin <21600108@handong.edu>
2020-02-12 11:41:22 +0900

    Revise file code - # file I/O, pushed condition

--- GIT_CLONE.py
+++ GIT_CLONE.py
@@ -23,7 +23,7 @@ def url_make(name, para_set, que_set, date_set):
         else:
             que_set[idx] = name + ":" + str(para_set[1])
             
-        if name == "pushed": # 날짜 기준만 재설정
+        if name == "pushed":
             if idx == -1:
                 date_set[0] = str(len(que_set) - 1)
             else:
@@ -39,7 +39,7 @@ def url_make(name, para_set, que_set, date_set):
         else:
             que_set[idx] = name + ":" + str(para_set[1]) + ".." + str(para_set[2])
             
-        if name == "pushed": # 날짜 기준만 재설정
+        if name == "pushed":
             if idx == -1:
                 date_set[0] = str(len(que_set) - 1)
             else:
@@ -49,15 +49,15 @@ def url_make(name, para_set, que_set, date_set):
             date_set[2] = str(para_set[2])
 
 
-username = "yeongbin123"
-token = "d302d70de2034265895446a2510663d38bd8d97c"
+username = "yeongbin123" # input("Your name here : ")
+token = "dfd2fe71b58f73ee77137c659096a89589b27378" # input("Your token here : ")
 
 gh_session = requests.Session()
 gh_session.auth = (username, token)
 
 # command line
 
-url = "https://api.github.com/search/repositories?q=" # Basic form
+url = "https://api.github.com/search/repositories?q="
 
 print("1. Language\t2. Created\t3. Pushed\t4. Forks\n5. Stars\t6. Sizes\t7. Followers\t8. Topics")
 
@@ -65,23 +65,23 @@ com = input("Input your command that you want to search(Search : 0) : ")
 print("")
 
 
-stand_date = "2008-01-01"
+stand_date = "2007-01-01"
 limit_date = str(datetime.date.today())
 date_set = ["-1", stand_date, limit_date]
 que_set = []
 
 while not com[0] == "0":
-    if com[0] == "1": # 이부분 바꿔야함 
+    if com[0] == "1":
         try:
             list_com = com.split(" ")
-            url += "language:" + str(list_com[1])
+            url_make("language", list_com, que_set, date_set)
         except:
             print("Check command form")
     elif com[0] == "2":
         try:
             list_com = com.split(" ")
             url_make("created", list_com, que_set, date_set)
-        except Exception as es:
+        except:
             print("Check command form", es)
     elif com[0] == "3":
         try:
@@ -137,7 +137,7 @@ flag = 1
 page_che = 0 
 
 if date_set[0] == "-1":
-    url_make("created", date_set, que_set, date_set)
+    url_make("pushed", date_set, que_set, date_set)
 
 org_url = url # Org url store
 
@@ -145,66 +145,83 @@ stand_date = date_set[1]
 
 limit_date = date_set[2]
 
-# print(stand_date, limit_date)
-
 start = timeit.default_timer()
 
-while stand_date <= limit_date: # Stand date -> Limit date
+total_che = 1
+
+set_date = limit_date
+
+repos_list = []
+
+repos_list = set()
+
+try:
+  f = open("output.txt",'w')
+except IOError as e:
+  print("IOError: {0}".format(e))
+
+while int(total_che) != 0:
     url = org_url 
     
     che = 0 # Count check
     
     for ss in que_set:
-        if che != int(date_set[0]): # date_set[0] == Created query
+        if che != int(date_set[0]): # date_set[0] == pushed query
             url += "+" + str(ss)
             
         che += 1 
         
-    url += "+" + str(que_set[int(date_set[0])]) + "&per_page=100&page=" # Url create
+    url += "+" + str(que_set[int(date_set[0])]) + "&sort=updated&order=desc&per_page=100&page=" # Url create
        
-    while idx_n == 101: # Data show 
+    while idx_n == 101 and page != 11: # Data show 
         set_url = url + str(page) # Pagination
-        # print(set_url)
-        if page_che % 20 == 0 and page_che != 0: # Limit 30 
+        print(set_url)
+        if page_che % 30 == 0 and page_che != 0: # Limit 30 
             sleep(60)
             page_che = 0 
         
         request = json.loads(gh_session.get(set_url).text) # Use json type to parse
+
+        total_che = int(request['total_count'])
+
         page_che += 1
-        """
-        if flag: # Total count check
-            print("<Total_count is " + str(request['total_count']) + ">")
-            
-            flag = 0
-        """ 
+
         idx_n = 1
-        # print(stand_date, limit_date)
         
         try:
             for repo in request['items']:
-                    print(str(total+idx_n)+" "+repo['html_url'])
-     
+                    rem = len(repos_list)
+                    repos_list.add(repo['html_url'])
+                    if rem != len(repos_list):
+                        f.write(repo['html_url'] + '\n')
+                    set_date = repo['pushed_at']
                     idx_n += 1
-        except:
+        except Exception as ee:
+            print(ee)
             break
         
         page += 1
-        
-        total = total + idx_n - 1
 
     page = 1 # Reset
 
     idx_n = 101
 
     flag = 1
-    
-    bef_date = datetime.datetime.strptime(stand_date, "%Y-%m-%d").date()
-    
-    aft_date = bef_date + datetime.timedelta(days=1)
-            
-    que_set[int(date_set[0])] = "created" + ":" + str(bef_date) + "T00:00.." + str(aft_date) + "T00:00"
 
-    stand_date = str(aft_date)
+    if str(set_date) == limit_date:
+        break
+    
+    if que_set[int(date_set[0])][7] == '<' or que_set[int(date_set[0])][7] == '>':
+        que_set[int(date_set[0])] = que_set[int(date_set[0])][:8] + str(set_date)
+    else:
+        que_set[int(date_set[0])] = que_set[int(date_set[0])][:7] + stand_date + ".." + str(set_date)
+
+    limit_date = str(set_date)
 
 stop = timeit.default_timer()
+
 print("Total_time : " + str((stop - start) / 60))
+
+print("Total_repos_count : " + str(len(repos_list)))
+
+f.close()
